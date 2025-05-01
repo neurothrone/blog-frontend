4131dabbadf49a97f1e661e2bdda61d3b8a56320
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-05-01 19:51:00 +0200

    add authentication logic and forms to register and login pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,59 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<null | string>(null);
+
+  const navigate = useNavigate();
+
+  async function handleLogin() {
+    setError(null);
+
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      navigate("/posts");
+    } catch (error: unknown) {
+      setError(error instanceof Error ? error.message : "Something went wrong");
+    }
+  }
+
   return (
-    <div>
+    <>
       <h1>Log In</h1>
-    </div>
+      {error && <p>{error}</p>}
+      <input
+        type="email"
+        placeholder="Email"
+        required
+        autoFocus
+        autoComplete="off"
+        spellCheck="false"
+        autoCapitalize="off"
+        autoCorrect="off"
+        pattern="[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*"
+        title="Enter a valid email address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}/>
+      <input
+        type="password"
+        placeholder="Password"
+        required
+        minLength={6}
+        maxLength={20}
+        pattern="[a-zA-Z0-9 ]+"
+        title="Only letters and spaces are allowed"
+        autoComplete="off"
+        spellCheck="false"
+        autoCapitalize="off"
+        autoCorrect="off"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}/>
+      <button onClick={handleLogin}>Log In</button>
+      <Link to="/register">Register</Link>
+    </>
   );
 };
 
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,8 +1,79 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState<null | string>(null);
+
+  const navigate = useNavigate();
+
+  async function handleRegister() {
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    setError(null);
+
+    try {
+      await createUserWithEmailAndPassword(getAuth(), email, password);
+      navigate("/posts");
+    } catch (error: unknown) {
+      setError(error instanceof Error ? error.message : "Something went wrong");
+    }
+  }
+
   return (
-    <div>
+    <>
       <h1>Register</h1>
-    </div>
+      {error && <p>{error}</p>}
+      <input
+        type="email"
+        placeholder="Email"
+        required
+        autoFocus
+        autoComplete="off"
+        spellCheck="false"
+        autoCapitalize="off"
+        autoCorrect="off"
+        pattern="[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*"
+        title="Enter a valid email address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}/>
+      <input
+        type="password"
+        placeholder="Password"
+        required
+        minLength={6}
+        maxLength={20}
+        pattern="[a-zA-Z0-9 ]+"
+        title="Only letters and spaces are allowed"
+        autoComplete="off"
+        spellCheck="false"
+        autoCapitalize="off"
+        autoCorrect="off"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}/>
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        required
+        minLength={6}
+        maxLength={20}
+        pattern="[a-zA-Z0-9 ]+"
+        title="Only letters and spaces are allowed"
+        autoComplete="off"
+        spellCheck="false"
+        autoCapitalize="off"
+        autoCorrect="off"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}/>
+      <button onClick={handleRegister}>Register</button>
+      <Link to="/login">Log In</Link>
+    </>
   );
 };
 
