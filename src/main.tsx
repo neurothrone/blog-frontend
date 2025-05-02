eff1ad47a8abb516dd836ff90c4bf70bdd504d5a
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-05-02 03:57:30 +0200

    add firebase initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,22 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { initializeApp } from "firebase/app";
 import App from "./App.tsx"
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
